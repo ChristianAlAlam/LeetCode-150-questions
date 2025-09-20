798f14f4d68f05f500d5dbe24e2db678853f033c
Christian Al Alam <christian.m.alam@gmail.com>
2025-09-20 16:46:27 +0300

    Solved the 'Merge Sorted Array' problem

---
+++ Merge Sorted Array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int min;
+        for(int i = 0; i<n; i++){
+            nums1[m+i] = nums2[i];
+        }
+
+        for(int i = 0; i<m+n; i++){
+            min = i;
+            for(int j = i+1; j<m+n; j++){
+                if(nums1[min] > nums1[j]){
+                    min = j;
+                }
+            }
+            int temp = nums1[i];
+            nums1[i] = nums1[min];
+            nums1[min] = temp;
+        }
+    }
+}
+
+// or simply
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int min;
+        for(int i = 0; i<n; i++){
+            nums1[m+i] = nums2[i];
+        }
+
+        Arrays.sort(nums1);
+    }
+}
